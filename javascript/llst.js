62f5d480466208f2dc7f042d714c1273fa81c1ca
kennankole <kennankole@gmail.com>
2022-07-12 11:03:03 +0300

    Implemented a singly linked list in javascript - Created a LinkedList class - Implemented an insert and a display method in the LinkedList class

--- javascript/llst.js
+++ javascript/llst.js
@@ -1,7 +1,41 @@
 class Node{
-    constructor(data, next=None){
+    constructor(data, next=null){
         this.data = data;
         this.next = next;
     }
 }
 
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+    }
+    insert(data){
+        let newNode = new Node(data)
+        if(this.head){
+            let currentNode = this.head
+            while(currentNode.next){
+                currentNode = currentNode.next
+            }
+            currentNode.next = newNode;
+        }else{
+            this.head = newNode;
+        }
+    }
+
+    display(){
+        let currentHead = this.head
+        while(currentHead){
+            console.log(currentHead.data)
+            currentHead = currentHead.next
+        }
+    }
+}
+
+
+let one = new LinkedList()
+one.insert(50)
+one.insert(90)
+one.insert(40)
+
+one.display()
