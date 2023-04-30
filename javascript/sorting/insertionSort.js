177987ca2c61ebf94e19b867665582977ca5f2ea
kennankole <kennankole@gmail.com>
2023-04-30 11:50:29 +0300

    Implemented insertion sort algorithm, descending order, in JavaScript.

--- javascript/sorting/insertionSort.js
+++ javascript/sorting/insertionSort.js
@@ -10,5 +10,19 @@ const insertionSortAsc = (arr) => {
   }
   return arr
 }
+
+const insertionSortDesc = (arr) => {
+  for(let i = 0; i < arr.length; i++){
+    currentIndex = i;
+    while(currentIndex > 0 && arr[currentIndex] > arr[currentIndex - 1]){
+      temp = arr[currentIndex];
+      arr[currentIndex] = arr[currentIndex - 1];
+      arr[currentIndex - 1] = temp;
+      currentIndex -= 1;
+    }
+  }
+  return arr;
+}
 const arr = [5, 3, 1, 2, 4]
 console.log(insertionSortAsc(arr))
+console.log(insertionSortDesc(arr))
