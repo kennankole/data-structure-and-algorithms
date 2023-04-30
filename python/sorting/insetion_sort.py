53d622ab6dce35dd57d5051a1d9168df23701ac2
kennankole <kennankole@gmail.com>
2023-04-30 11:42:08 +0300

    Implemented insertion sort, descending in Python.

--- python/sorting/insetion_sort.py
+++ python/sorting/insetion_sort.py
@@ -1,4 +1,4 @@
-def sorting_list(arr):
+def sorting_list_asc(arr):
   for idx, val in enumerate(arr):
     current_idx = idx
     while current_idx > 0 and arr[current_idx] < arr[current_idx - 1]:
@@ -6,5 +6,14 @@ def sorting_list(arr):
       current_idx -= 1
   return arr
 
+def sorting_list_dsc(arr):
+  for idx, value in enumerate(arr):
+    current_idx = idx
+    while current_idx > 0 and arr[current_idx] > arr[current_idx - 1]:
+      arr[current_idx - 1], arr[current_idx] = arr[current_idx], arr[current_idx - 1]
+      current_idx -= 1
+  return arr
+
 arr = [5, 3, 1, 2, 4]
-print(sorting_list(arr=arr))
+print(sorting_list_asc(arr=arr))
+print(sorting_list_dsc(arr=arr))
