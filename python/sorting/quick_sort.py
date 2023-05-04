f0a885d9913cafb5275580968543c0b22d18fb03
kennankole <kennankole@gmail.com>
2023-05-04 09:33:09 +0300

    Implemented quicksort sorting algorithm in Python to sort lists in ascending and descending order.

---
+++ python/sorting/quick_sort.py
@@ -0,0 +1,25 @@
+def quicksort_middle_pivot_asc(arr):
+  if len(arr) <= 1:
+    return arr
+  
+  pivot = arr[len(arr) // 2]
+  left = [x for x in arr if x < pivot]
+  middle = [x for x in arr if x == pivot]
+  right = [x for x in arr if x > pivot]
+  return quicksort_middle_pivot_asc(left) + middle + quicksort_middle_pivot_asc(right)
+
+def quicksort_middle_pivot_desc(arr):
+  if len(arr) <= 1:
+    return arr
+  pivot = arr[len(arr) // 2]
+  left = [x for x in arr if x < pivot]
+  middle = [x for x in arr if x == pivot]
+  right = [x for x in arr if x > pivot]
+  return quicksort_middle_pivot_desc(right) + middle + quicksort_middle_pivot_desc(left)
+
+
+
+arr = [5, 3, 1, 2, 4]
+print(quicksort_middle_pivot_asc(arr))
+print(quicksort_middle_pivot_desc(arr))
+
