a037e7dcd3511fa051b1d194524bc3983f1614fd
kennankole <kennankole@gmail.com>
2023-05-05 08:35:44 +0300

    Implemented binary search in Python

---
+++ python/binary_search/bst.py
@@ -0,0 +1,16 @@
+def binary_search(arr, target):
+  left = 0
+  right = len(arr) - 1
+  while left <= right:
+    mid = (left + right) // 2
+    if arr[mid] == target:
+      return arr[mid]
+    elif arr[mid] < target:
+      left = mid + 1
+    else:
+      right = mid - 1
+  return - 1
+
+arr = [1, 3, 5, 7, 8]
+target = 5
+print(binary_search(arr=arr, target=8))
