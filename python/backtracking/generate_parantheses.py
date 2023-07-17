feb5c46f6a4c840cbd9673fd5172a2516774b319
kennankole <kennankole@gmail.com>
2023-07-17 09:57:17 +0300

    Generate valid paranthese problem with dfs backtracking

--- python/backtracking/generate_parantheses.py
+++ python/backtracking/generate_parantheses.py
@@ -26,4 +26,23 @@ def generate_parantheses(n: int) -> List[str]:
   backtracking(0, 0)
   return ans
 
-print(generate_parantheses(3))
+
+# Using dfs
+def generate_paranthese_combination(n):
+  def dfs(start_index, path, open_count, close_count, res):
+    if start_index == 2 * n:
+      res.append(''.join(path))
+      return
+    if open_count < n:
+      path.append('(')
+      dfs(start_index + 1, path, open_count + 1, close_count, res)
+      path.pop()
+    if close_count < open_count:
+      path.append(')')
+      dfs(start_index + 1, path, open_count, close_count + 1, res)
+      path.pop()
+  ans = []
+  dfs(0, [], 0, 0, ans)
+  return ans
+
+print(generate_paranthese_combination(3))
